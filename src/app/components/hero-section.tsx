c978d950a2e642b92c05d877cc395dd1ee77b99e
Mehakanis <159849439+Mehakanis@users.noreply.github.com>
2024-12-06 10:25:55 +0500

    updated

--- src/app/components/hero-section.tsx
+++ src/app/components/hero-section.tsx
@@ -62,7 +62,7 @@ export default function HeroSection() {
       {/* Content over the background */}
       <div className="relative z-10 flex items-center ">
         <Image
-          src={"/image/hero.png"}
+          src={"/hero.png"}
           alt="Scooter Image"
           width={800}
           height={800}
